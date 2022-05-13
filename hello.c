c2672a7b047c103d3262481e2174c91ddaefe2f2
lgh877 <lgh877@naver.com>
2022-05-12 23:31:40 -0700

    Holy

---
+++ hello.c
@@ -0,0 +1,4 @@
+#include <stdio.h>
+#include "myheader.h"
+
+void printHello(){printf("Hello world!\n");}
---
+++ main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "myheader.h"
+
+int main(){
+    printHello();
+    printBye();
+    return 0;
+}
